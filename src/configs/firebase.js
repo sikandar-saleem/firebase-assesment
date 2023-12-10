96c14dc48a8e5fc044ed07ee16d8987f7abca6d7
Sikandar Saleem <sikander.saleem@camp1.tkxel.com>
2023-12-10 09:21:37 +0500

    Use ENV variables

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -1,15 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
-// TODO: Move this to ENV
 const firebaseConfig = {
-  apiKey: "AIzaSyDQXR6aTRio85lRF6_oiM694LDSHSQqRYM",
-  authDomain: "fir-assesment-bd992.firebaseapp.com",
-  projectId: "fir-assesment-bd992",
-  storageBucket: "fir-assesment-bd992.appspot.com",
-  messagingSenderId: "955680673543",
-  appId: "1:955680673543:web:8a72e06058c876ec65bfa2",
-  measurementId: "G-NFHPFBSW07",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
